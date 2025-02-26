07c739a47abc8ddb2f78dee908b3ee79a48c42f0
zeropointnothing <ZeroPointNothing@outlook.com>
2025-02-26 11:58:25 -0500

    add javadoc

--- src/com/jpowder/powder/Registry.java
+++ src/com/jpowder/powder/Registry.java
@@ -3,8 +3,13 @@ package com.jpowder.powder;
 import java.util.ArrayList;
 import java.util.Objects;
 
+/**
+ * Powder Registry.
+ * <p/>
+ * Holds registered powders so that they can be cloned later.
+ */
 public class Registry {
-    private ArrayList<RegistryEntry> registeredPowder;
+    private final ArrayList<RegistryEntry> registeredPowder;
 
         /**
          * Registry entry, used for storing BasePowder objects and an ID.
@@ -15,10 +20,24 @@ public class Registry {
     public Registry() {
         registeredPowder = new ArrayList<RegistryEntry>();
     }
+
+
+    /**
+     * Register a new powder.
+     * @param powder The instance of the powder to clone
+     * @param id The id to set the powder to
+     */
     public void register(BasePowder powder, String id) {
         registeredPowder.add(new RegistryEntry(powder, id));
     }
-    public BasePowder createInstance(String id) {
+
+    /**
+     * Create a new instance of a powder.
+     * @param id The id of the powder to create
+     * @return a clone of the powder
+     * @throws RuntimeException if the requested powder hasn't been registered
+     */
+    public BasePowder createInstance(String id) throws RuntimeException {
         for (RegistryEntry entry : registeredPowder) {
             if (Objects.equals(entry.id, id)) {
                 return entry.powder.clone();
